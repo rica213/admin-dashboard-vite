9c133b4929266a1411d81335724b1127593ebb91
rica213 <25f5k252l@mozmail.com>
2023-08-17 19:29:51 +0300

    Implement footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,10 @@ import './footer.scss'
 
 const Footer = () => {
   return (
-    <div  className='footer'>Footer</div>
+    <div  className='footer'>
+      <span>AdminDash</span>
+      <span>© Tao Dashboard</span>
+    </div>
   )
 }
 
